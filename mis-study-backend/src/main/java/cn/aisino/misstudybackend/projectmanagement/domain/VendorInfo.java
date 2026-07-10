54c8dbee62a688a963c21951c7374b9069d37cf8
bearhero123 <694218912@qq.com>
2026-07-10 16:54:07 +0800

    供应商实体类

---
+++ mis-study-backend/src/main/java/cn/aisino/misstudybackend/projectmanagement/domain/VendorInfo.java
@@ -0,0 +1,36 @@
+package cn.aisino.misstudybackend.projectmanagement.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@TableName("vendor_info")
+
+public class VendorInfo {
+
+    @TableId(value = "vendor_id",type = IdType.AUTO)
+
+    private Long vendorId;
+
+    private String vendorName;
+    private String contactName;
+    private String contactPosition;
+    private String contactPhone;
+    private String contactEmail;
+    private String vendorRatings;
+    private String vendorAddr;
+    private String remark;
+
+    private String createBy;
+    private LocalDateTime createTime;
+    private String updateBy;
+    private LocalDateTime updateTime;
+
+    private String delFlag;
+
+}
